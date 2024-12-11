11962a0d7d698d9305c674401c5db88bdf5a8142
BivasOffice <bivas.digiex@gmail.com>
2024-12-11 14:43:59 +0530

    Status Add For Product Verient

--- resources/views/admin/maincontents/product/edit.blade.php
+++ resources/views/admin/maincontents/product/edit.blade.php
@@ -46,24 +46,24 @@
               <div class="card-body">
                 <div class="col-md-12 text-center" id="statusec">
                     @if($total_varient > 0)
-                    @if($product->status=="0")
-                    <span class="text-info">
-                      <p>
-                      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-info-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M12 9h.01" /><path d="M11 12h1v4h1" /></svg> 
-                       Publish the product to display in the listing page.
-                      </p>
-                    </span>
+                      @if($product->status=="0")
+                        <span class="text-info">
+                          <p>
+                          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-info-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M12 9h.01" /><path d="M11 12h1v4h1" /></svg> 
+                          Publish the product to display in the listing page.
+                          </p>
+                        </span>
+                      @else
+                        <span class="text-success">
+                          <p><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-circle-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M9 12l2 2l4 -4" /></svg>
+                          Product Published</p>
+                        </span>
+                      @endif
+                      <select class="custom-select" id="slstatus">
+                        <option value="0" {{ ($product->status=="0") ? 'selected="selected"' : "" }}>Unpublished</option>
+                        <option value="1" {{ ($product->status=="1") ? 'selected="selected"' : "" }}>Published</option>
+                      </select>
                     @else
-                    <span class="text-success">
-                      <p><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-circle-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M9 12l2 2l4 -4" /></svg>
-                      Product Published</p>
-                    </span>
-                    @endif
-                    <select class="custom-select" id="slstatus">
-                      <option value="0" {{ ($product->status=="0") ? 'selected="selected"' : "" }}>Unpublished</option>
-                      <option value="1" {{ ($product->status=="1") ? 'selected="selected"' : "" }}>Published</option>
-                    </select>
-                        @else
                       <div class="text-warning">
                         <p><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-alert-triangle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 9v4" /><path d="M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z" /><path d="M12 16h.01" /></svg>
                           Please add Varient To Publish Your Product.</p>
